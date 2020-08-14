002c532b272187123da01aceded830216b7eaad4
bingoRgb <fangjibule@sina.cn>
2020-08-14 17:50:24 +0800

    修改了输入子系统代码

--- key_input/key_input.c
+++ key_input/key_input.c
@@ -35,6 +35,7 @@ struct keyInfo
 	char name[20];
 	irq_handler_t handlerFunc;//中断处理函数
 	struct timer_list timer;
+	unsigned int keyCode;		//上报输入子系统的按键索引号
 };
 
 
@@ -54,106 +55,9 @@ struct _keyInputInfo
 };
 struct _keyInputInfo keyInputInfo;
 
-static int key_interrupt_open(struct inode *inode, struct file *file)
-{
-	file->private_data = (struct keyInputInfo*)&keyInputInfo;
-	return 0;
-}
-
-static ssize_t key_interrupt_write(struct file *file, const char __user *data,
-			 						size_t len, loff_t *ppos)
-{
-	return 0;
-}
-
-int hasVaildKey(struct keyInputInfo* dev)
-{
-	int i;
-	for(i=0;i<KEY_NUM;i++)
-	{
-		if(atomic_read(&dev->key[i].pressFlag) == KEY_VALUE_RAISE)	//只要有一个按键输入有效
-		{
-			return 1;
-		}
-	}
-	return 0;
-}
-
-static ssize_t key_interrupt_read(struct file *file, char __user *userbuf,
-				  size_t count, loff_t *ppos)
-{
-	int result=0;
-	char value=0;
-	char noScheduleFlag=0;
-	int i;
-	struct keyInputInfo* dev = (struct keyInputInfo*)file->private_data;
-#if 0
-	DECLARE_WAITQUEUE(wait,current);	/* 定义一个等待队列项 */		    /* wait是这个队列项结构体变量的名字，看宏定义，不能是字符串类型 */
-#endif
-	for(i=0;i<KEY_NUM;i++)
-	{
-		if(atomic_read(&dev->key[i].pressFlag) == KEY_VALUE_RAISE)	//只要有一个按键输入有效
-		{
-			noScheduleFlag = 1;
-			goto vaild_key;
-		}
-	}
-
-	wait_event_interruptible(dev->r_wait, hasVaildKey(dev));	//可被信号打断的等待事件，是否有有效按键输入
-
-
-#if 0
-	/* 按键输入全部无效 */
-	add_wait_queue(&dev->r_wait,&wait);	//将等待队列项添加到等待队列头
-	__set_current_state(TASK_INTERRUPTIBLE);	//当前进程设置为可被信号打断的状态
-	schedule();                     /* 切换 */
-
-
-	/* 唤醒后，程序从这里继续执行 */
-	if(signal_pending(current))		//判断是信号唤醒还是中断唤醒
-	{
-		result = -ERESTARTSYS;
-		goto exit_onqueue;
-	}
-#endif
-
-vaild_key:
-	for(i=0;i<KEY_NUM;i++)
-	{
-		if(atomic_read(&dev->key[i].pressFlag) == KEY_VALUE_RAISE)
-		{
-			value |= (1<<i);
-			atomic_set(&dev->key[i].pressFlag,KEY_VALUE_NONE);
-		}	
-	}
-
- 	result = copy_to_user(userbuf,&value,1);
-#if 0
-	if(noScheduleFlag)	return result;	//没有进行任务调度
-	
-exit_onqueue:
-	__set_current_state(TASK_RUNNING);      /* 将当前任务设置为运行状态 */
-	remove_wait_queue(&dev->r_wait, &wait);    /* 将对应的队列项从等待队列头删除 */
-#endif
-	return result;
-}
-
-
-
-
-				  
-
-const struct file_operations key_interrupt_fops =
-{
-	.owner		= THIS_MODULE,
-	.open		= key_interrupt_open,
-	.read		= key_interrupt_read,
-	.write      = key_interrupt_write,
-};
-
 static irqreturn_t irq_handler_func_key_0(int irq, void *dev_id)
 {
-	struct keyInputInfo* dev = (struct keyInputInfo*)dev_id;
+	struct _keyInputInfo* dev = (struct _keyInputInfo*)dev_id;
 	atomic_set(&dev->key[0].keyValue,__gpio_get_value(dev->key[0].keyGpio));
 
 	/* 激活定时器进行消抖处理 */
@@ -183,7 +87,8 @@ static void timerKeyCallFunction_key_0(unsigned long data)
 			if(atomic_read(&dev->pressFlag) == KEY_VALUE_PRESS)
 			{
 				atomic_set(&dev->pressFlag,KEY_VALUE_RAISE);
-				wake_up(&keyInputInfo.r_wait);		//唤醒
+				input_report_key(keyInputInfo.input_key_dev,dev->keyCode,1);
+				input_sync(keyInputInfo.input_key_dev);
 			}
 			else
 			{
@@ -207,9 +112,7 @@ static void timerKeyCallFunction_key_0(unsigned long data)
 static irq_handler_t irqFuncArray[KEY_NUM] = {irq_handler_func_key_0};
 static void (*irqTimerArray[KEY_NUM])(unsigned long) = {timerKeyCallFunction_key_0};
 
-
-
-static int key_init_irq_io(struct keyInputInfo * dev)
+static int key_init_irq_io(struct _keyInputInfo * dev)
 {
 	int i;
 	int result = 0;
@@ -285,62 +188,57 @@ static int __init key_input_init(void)
 	}
 
 	keyInputInfo.input_key_dev->name = KEY_INPUT_NAME;
-	keyInputInfo.input_key_dev->evbit[0] = 
-
-
+	
+	__set_bit(EV_KEY, keyInputInfo.input_key_dev->evbit);
+	__set_bit(EV_REP, keyInputInfo.input_key_dev->evbit);
+	__set_bit(KEY_0,keyInputInfo.input_key_dev->keybit);
 	
 
-	result = key_init_irq_io(&keyInterruptInfo);
+	result = input_register_device(keyInputInfo.input_key_dev);
 	if(result < 0)
 	{
-		goto key_init_faild;
+		result = -EINVAL;
+		goto init_faild;
+	}
+	
+
+	result = key_init_irq_io(&keyInputInfo);
+	if(result < 0)
+	{
+		result = -EINVAL;
+		goto init_irq_io_faild;
 	}
 
-	init_waitqueue_head(&keyInterruptInfo.r_wait);	//初始化等待队列头
-	
+	keyInputInfo.key[0].keyCode = KEY_0;
+
 	return 0;
 
-key_init_faild:
-	device_destroy(keyInterruptInfo.class, keyInterruptInfo.devid);
-device_faild:
-	class_destroy(keyInterruptInfo.class);
-class_faild:
-	cdev_del(&keyInterruptInfo.cdev);	
+init_irq_io_faild:
+	input_unregister_device(keyInputInfo.input_key_dev);
 init_faild:
-	unregister_chrdev_region(keyInterruptInfo.devid, KEY_INTERRUPT_CNT);
+	input_free_device(keyInputInfo.input_key_dev);
 dev_faild:
 	return result;
 }
 
-
-
-static void __exit key_interrupt_exit(void)
+static void __exit key_input_exit(void)
 {
 	int i;
-	/* 1,删除字符设备 */
-	cdev_del(&keyInterruptInfo.cdev);
-
-	/* 2,注销设备号 */
-	unregister_chrdev_region(keyInterruptInfo.devid, KEY_INTERRUPT_CNT);
-
-	/* 3,摧毁设备 */
-	device_destroy(keyInterruptInfo.class, keyInterruptInfo.devid);
-
-	/* 4,摧毁类 */
-	class_destroy(keyInterruptInfo.class);
+	input_unregister_device(keyInputInfo.input_key_dev);
+	input_free_device(keyInputInfo.input_key_dev);
 
 	for(i=0;i<KEY_NUM;i++)
 	{
-		gpio_free(keyInterruptInfo.key[i].keyGpio);
-		free_irq(keyInterruptInfo.key[i].irqIndex,&keyInterruptInfo);
-		del_timer_sync(&keyInterruptInfo.key[i].timer);
+		gpio_free(keyInputInfo.key[i].keyGpio);
+		free_irq(keyInputInfo.key[i].irqIndex,&keyInputInfo);
+		del_timer_sync(&keyInputInfo.key[i].timer);
 	}
 	printk("**Kernel** : exti led driver succeed!!!\r\n");
 }
 
 
-module_init(key_interrupt_init);
-module_exit(key_interrupt_exit);
+module_init(key_input_init);
+module_exit(key_input_exit);
 
 
 
